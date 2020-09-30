836164cefc19c5c91d8c749006a0f67f4b7d7905
YuvalRotman46 <Yrotman2604@gmail.com>
2020-09-30 10:02:39 +0300

    Finished front-end and back-end - shity work

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask, render_template, request, make_response
 import classifier.classifier
+
+
+INDEX_PATH = 'index.html'
+RESPONSE_PATH = 'hotdog_response.html'
 
 app = Flask(__name__)
 
+detector = None
+try:
+    detector = classifier.classifier.Core()
+    detector.load_model()
+except NotImplementedError:
+    print("core is not implemented")
 
-@app.route('/')
-def hello_world():
-    return '<h1>Hello World !</h1>'
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_single_page():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            img_name = "image.{}".format(image.filename.split(".")[-1])
+            image.save(os.path.join("./static/uploads", img_name))
+            print("img saved !")
+            print("detecting img...")
+            result = detector.classify(f"../static/uploads/{img_name}")
+
+            return make_response(render_template(RESPONSE_PATH, img_name=img_name, hotdog_text=f"This is {result}"))
+
+    return render_template(INDEX_PATH)
+
+
+@app.route('/index.html', methods=['GET', 'POST'])
+def index():
+    return render_template(INDEX_PATH)
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index_no_extension():
+    return render_template(INDEX_PATH)
 
 
 if __name__ == '__main__':
-    try:
-        detector = classifier.Classifier()
-        detector.load_model()
-    except NotImplementedError:
-        print("core is not implemented")
     app.run()
---
+++ static/src/utils.js
@@ -0,0 +1,5 @@
+
+function change_input_file_name(){
+    let input_file = document.getElementById("iput-file");
+    let input_file_text = document.getElementById("input-file-text");
+}
